b02a107f401a030e99c257f6ee9a0bf9d656ff64
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:26:53 +0100

    Update exports.swift with full @_exported re-exports
    Re-export all semantic dependencies for consumers.

---
+++ Sources/Buffer Primitives/exports.swift
@@ -0,0 +1,11 @@
+// exports.swift
+// Re-export dependencies for consumers.
+
+@_exported import Binary_Primitives
+@_exported import Deque_Primitives
+@_exported import Handle_Primitives
+@_exported import Reference_Primitives
+@_exported import Index_Primitives
+@_exported import Input_Primitives
+@_exported import Bit_Primitives
+@_exported import Collection_Primitives
