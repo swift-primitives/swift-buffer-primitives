f3bf5226459dc705b722fe02e17d3d4b31c6d321
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-24 09:19:42 +0100

    WIP

--- Sources/Buffer Slab Primitives/Buffer.Slab.Bounded.swift
+++ Sources/Buffer Slab Primitives/Buffer.Slab.Bounded.swift
@@ -71,9 +71,9 @@ extension Buffer.Slab.Bounded where Element: ~Copyable {
     }
 }
 
-// MARK: - Sequence.Drain.Protocol
+// MARK: - Sequence.Drain.Protocol (~Copyable)
 
-extension Buffer.Slab.Bounded: Sequence.Drain.`Protocol` {
+extension Buffer.Slab.Bounded: Sequence.Drain.`Protocol` where Element: ~Copyable {
     @inlinable
     public mutating func drain(_ body: (consuming Element) -> Void) {
         header.bitmap.ones.forEach { bitIndex in
